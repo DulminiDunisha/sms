fc3ccf0f89ae25fb4d690ac73ca7642d4edec38a
themo <themo@DESKTOP-JOL0IK2>
2024-05-01 17:02:35 +0530

    test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,37 +37,49 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(279, 55);
+            label1.Location = new Point(319, 73);
             label1.Name = "label1";
-            label1.Size = new Size(224, 15);
+            label1.Size = new Size(281, 20);
             label1.TabIndex = 0;
             label1.Text = "SUPERMARCKET MANAGEMENT SYSTEM";
             // 
             // button1
             // 
-            button1.Location = new Point(337, 149);
+            button1.Location = new Point(385, 199);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(122, 57);
+            button1.Size = new Size(139, 76);
             button1.TabIndex = 1;
             button1.Text = "login";
             button1.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(581, 294);
+            checkBox1.Location = new Point(424, 141);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(81, 24);
             checkBox1.TabIndex = 2;
             checkBox1.Text = "dulmini";
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            comboBox1.Location = new Point(0, 0);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 28);
+            comboBox1.TabIndex = 0;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(1045, 800);
             Controls.Add(checkBox1);
             Controls.Add(button1);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -79,5 +91,6 @@
         private Label label1;
         private Button button1;
         private CheckBox checkBox1;
+        private ComboBox comboBox1;
     }
 }
